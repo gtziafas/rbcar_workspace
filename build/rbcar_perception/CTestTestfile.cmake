9b136c572049ef4186576d42be963b1f4bf8b01b
gtziafas <pullmypill@hotmail.com>
2018-05-03 14:36:55 +0300

    Add files via upload

---
+++ build/rbcar_perception/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ggtz/rbcar_ws/src/rbcar_perception
+# Build directory: /home/ggtz/rbcar_ws/build/rbcar_perception
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
