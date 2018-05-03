ddcd16721cd902a26d8e4fa726b9a77920d373d1
gtziafas <pullmypill@hotmail.com>
2018-05-03 15:12:12 +0300

    Add files via upload

---
+++ build/rbcar_sim/rbcar_joystick/cmake_install.cmake
@@ -0,0 +1,49 @@
+# Install script for directory: /home/ggtz/rbcar_ws/src/rbcar_sim/rbcar_joystick
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/home/ggtz/rbcar_ws/install")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig" TYPE FILE FILES "/home/ggtz/rbcar_ws/build/rbcar_sim/rbcar_joystick/catkin_generated/installspace/rbcar_joystick.pc")
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/rbcar_joystick/cmake" TYPE FILE FILES
+    "/home/ggtz/rbcar_ws/build/rbcar_sim/rbcar_joystick/catkin_generated/installspace/rbcar_joystickConfig.cmake"
+    "/home/ggtz/rbcar_ws/build/rbcar_sim/rbcar_joystick/catkin_generated/installspace/rbcar_joystickConfig-version.cmake"
+    )
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/rbcar_joystick" TYPE FILE FILES "/home/ggtz/rbcar_ws/src/rbcar_sim/rbcar_joystick/package.xml")
+endif()
+
