b0b56085d95d047e67ba71e5b67e7b936fb7ad75
gtziafas <pullmypill@hotmail.com>
2018-05-03 15:15:58 +0300

    Add files via upload

---
+++ build/rbcar_sim/rbcar_joystick/CMakeFiles/rbcar_joystick_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rbcar_joystick_node.dir/src/rbcar_joystick.cpp.o"
+  "/home/ggtz/rbcar_ws/devel/lib/rbcar_joystick/rbcar_joystick_node.pdb"
+  "/home/ggtz/rbcar_ws/devel/lib/rbcar_joystick/rbcar_joystick_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rbcar_joystick_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
