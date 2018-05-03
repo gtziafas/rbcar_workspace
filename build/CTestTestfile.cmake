f6ac9080c3c73deada35ae76c3d5d89697b8628a
gtziafas <pullmypill@hotmail.com>
2018-05-03 14:20:29 +0300

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,24 @@
+# CMake generated Testfile for 
+# Source directory: /home/ggtz/rbcar_ws/src
+# Build directory: /home/ggtz/rbcar_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(rbcar_common/rbcar_common)
+subdirs(rbcar_sim/rbcar_sim)
+subdirs(rbcar_sim/rbcar_sim_bringup)
+subdirs(msgs/ackermann_msgs)
+subdirs(msgs/robotnik_msgs)
+subdirs(teb_local_planner_tutorials)
+subdirs(rbcar_sim/rbcar_joystick)
+subdirs(rbcar_common/rbcar_pad)
+subdirs(rbcar_sim/rbcar_robot_control)
+subdirs(rbcar_perception)
+subdirs(depthcloud_encoder)
+subdirs(tutorials)
+subdirs(rbcar_sim/rbcar_control)
+subdirs(rbcar_common/robotnik_sensors)
+subdirs(rbcar_common/rbcar_description)
+subdirs(rbcar_sim/rbcar_gazebo)
+subdirs(rbcar_navigation)
