2efacd166517a0ced2722a7e2688366e01974f74
gtziafas <pullmypill@hotmail.com>
2018-05-03 14:50:50 +0300

    Add files via upload

---
+++ devel/share/ackermann_msgs/cmake/ackermann_msgs-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(ackermann_msgs_MSG_INCLUDE_DIRS "/home/ggtz/rbcar_ws/src/msgs/ackermann_msgs/msg")
+set(ackermann_msgs_MSG_DEPENDENCIES std_msgs)
