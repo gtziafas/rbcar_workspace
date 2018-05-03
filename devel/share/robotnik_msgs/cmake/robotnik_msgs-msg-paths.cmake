4516ed152e43cb5fbd407dcdacb419051c8a8dac
gtziafas <pullmypill@hotmail.com>
2018-05-03 14:55:50 +0300

    Add files via upload

---
+++ devel/share/robotnik_msgs/cmake/robotnik_msgs-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(robotnik_msgs_MSG_INCLUDE_DIRS "/home/ggtz/rbcar_ws/src/msgs/robotnik_msgs/msg")
+set(robotnik_msgs_MSG_DEPENDENCIES std_msgs)
